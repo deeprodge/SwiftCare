be85ba7779933ca5e02b6d496cd289cd6b514fa4
deeprodge <deeprodge14@gmail.com>
2024-09-28 20:33:25 -0700

    Initial Commit

---
+++ EyeTrac/EyeTracApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeTracApp.swift
+//  EyeTrac
+//
+//  Created by Deep Rodge on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct EyeTracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EyeTracTests/EyeTracTests.swift
@@ -0,0 +1,16 @@
+//
+//  EyeTracTests.swift
+//  EyeTracTests
+//
+//  Created by Deep Rodge on 9/28/24.
+//
+
+import Testing
+
+struct EyeTracTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
